c69991feaf131359c82b1a1f3c29d80ea6cfbf3f
mommyrex125-web <mommyrex125@gmail.com>
2025-10-01 09:24:47 +0630

    Second CI

---
+++ SDLC/src/sdlceclipse/Cjava.java
@@ -0,0 +1,11 @@
+package sdlceclipse;
+
+public class Cjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Contious Integration");
+
+	}
+
+}
