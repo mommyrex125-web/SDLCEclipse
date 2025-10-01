d4aa7006eaa31774e1290a043c5676b99a21798a
mommyrex125-web <mommyrex125@gmail.com>
2025-10-01 09:02:21 +0630

    Initial setup Continuous Integration

---
+++ SDLC/src/sdlceclipse/HelloSDLC.java
@@ -0,0 +1,12 @@
+package sdlceclipse;
+
+public class HelloSDLC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Continous Integration");
+		
+
+	}
+
+}
